eb04b18f5a922ce0c49e2c6db6a2dc80ab7747e8
Maciej <maciej.gbiorczyk@student.put.poznan.pl>
2017-11-16 11:05:55 +0100

    MG ScoreSamples.java - generated equals() and hashCode() methods.

--- SpeakerRecognition/src/main/java/speakerrecognition/pojos/ScoreSamples.java
+++ SpeakerRecognition/src/main/java/speakerrecognition/pojos/ScoreSamples.java
@@ -1,5 +1,7 @@
 package speakerrecognition.pojos;
 
+import java.util.Arrays;
+
 public class ScoreSamples {
 	private double[][] data;
 	private double[] logLikelihoods;
@@ -56,4 +58,45 @@ public class ScoreSamples {
 	public double[] getWeights() {
 		return weights;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.deepHashCode(covars);
+		result = prime * result + Arrays.deepHashCode(data);
+		result = prime * result + Arrays.hashCode(logLikelihoods);
+		result = prime * result + Arrays.hashCode(logprob);
+		result = prime * result + Arrays.deepHashCode(means);
+		result = prime * result + Arrays.deepHashCode(responsibilities);
+		result = prime * result + Arrays.hashCode(weights);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ScoreSamples other = (ScoreSamples) obj;
+		if (!Arrays.deepEquals(covars, other.covars))
+			return false;
+		if (!Arrays.deepEquals(data, other.data))
+			return false;
+		if (!Arrays.equals(logLikelihoods, other.logLikelihoods))
+			return false;
+		if (!Arrays.equals(logprob, other.logprob))
+			return false;
+		if (!Arrays.deepEquals(means, other.means))
+			return false;
+		if (!Arrays.deepEquals(responsibilities, other.responsibilities))
+			return false;
+		if (!Arrays.equals(weights, other.weights))
+			return false;
+		return true;
+	}
+
 }
