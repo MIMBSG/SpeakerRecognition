45510104bf04086c967dca572a2bb10d6233a495
Maciej <maciej.gbiorczyk@student.put.poznan.pl>
2017-11-19 20:17:54 +0100

    MG Fetched from Master (MB)

--- SpeakerRecognition/src/main/java/speakerrecognition/services/interfaces/MfccService.java
+++ SpeakerRecognition/src/main/java/speakerrecognition/services/interfaces/MfccService.java
@@ -1,7 +1,21 @@
 package speakerrecognition.services.interfaces;
 
+import speakerrecognition.exceptions.MatrixesServiceException;
+import speakerrecognition.exceptions.MfccServiceException;
+import speakerrecognition.pojos.MfccParameters;
 
 public interface MfccService {
-	
-	
+
+	MfccParameters extractMfcc(int[] samplesVector, int fs) throws MfccServiceException, MatrixesServiceException;
+
+	double[] arrange(int lowLimit, int highLimit) throws MfccServiceException;
+
+	double energyOfVector(double[] vector);
+
+	double[] preemphasis(double[] vector, double preEmph);
+
+	double[][] dctMatrix(int size, int mfccNum) throws MatrixesServiceException;
+
+	double[][] melFilterBank(int numOfFilterBanks, int fftLength, int sampleRate) throws MfccServiceException;
+
 }
