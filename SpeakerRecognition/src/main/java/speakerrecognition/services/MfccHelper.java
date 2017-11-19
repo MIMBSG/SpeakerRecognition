ff8f491c0a0bc56faacb77c0bdff00f6cd638de9
MB <michal.bociek@student.put.poznan.pl>
2017-11-19 19:25:28 +0100

    MB - reworked MFCC

---
+++ SpeakerRecognition/src/main/java/speakerrecognition/services/MfccHelper.java
@@ -0,0 +1,173 @@
+package speakerrecognition.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import speakerrecognition.exceptions.MatrixesServiceException;
+import speakerrecognition.exceptions.MfccServiceException;
+import wavprocessing.MFCC;
+import wavprocessing.Matrixes;
+
+public class MfccHelper {
+	@Autowired
+	private MatrixesService matrixService;
+	
+	private static final String wrongLimitsError = "Low limit can't be higher than high limit!";
+	
+	public double[] arrange(int lowLimit, int highLimit) throws MfccServiceException
+	{
+		arrayLimitCheck(lowLimit, highLimit);
+		double[] result = new double[highLimit - lowLimit];
+		for(int i=0;i<result.length;i++){
+			result[i] = lowLimit+i;
+		}
+		
+		return result;
+	}
+	
+	public double energyOfVector(double[] vector)
+	{
+		double energy = 0;
+		for(int i=0; i<vector.length;i++){
+			energy = energy + Math.pow(vector[i], 2);
+		}
+		return energy;
+	}
+	
+	public double[] preemphasis(double[] vector, double preEmph){
+		double[] tempVector = new double[vector.length];
+		tempVector[0] = vector[0];
+		for(int i=1;i<vector.length;i++){
+			tempVector[i] = vector[i]-preEmph*vector[i-1];
+		}
+		return tempVector;
+	}
+	
+	public double[][] dctMatrix(int size, int mfccNum) throws MatrixesServiceException{
+		
+		double[][] d1Matrix = new double[size][size];
+		double[][] xMeshgridMatrix = new double[size][size];
+		for(int i=0;i<size;i++){
+			for(int j=0;j<size;j++){
+				xMeshgridMatrix[j][i] = i;
+			}
+		}
+		double[][] yMeshgridMatrix = new double[size][size];
+		for(int i=0;i<size;i++){
+			for(int j=0;j<size;j++){
+				yMeshgridMatrix[i][j] = i;
+			}
+		}
+		
+		d1Matrix = matrixService.multiplyMatrixesElementByElement(xMeshgridMatrix, yMeshgridMatrix);
+		
+		for(int i=0;i<size;i++){
+			for(int j=0;j<size;j++){
+				d1Matrix[i][j] = Math.sqrt(2/(double)size)*Math.cos(d1Matrix[i][j]);
+			}
+		}
+		for(int i=0;i<size;i++){
+			d1Matrix[0][i] /= Math.sqrt(2);
+		}
+		double[][] resultMatrix = new double[mfccNum][size];
+		for(int i=1;i<mfccNum+1;i++){
+			for(int j=0;j<size;j++){
+				resultMatrix[i-1][j] = d1Matrix[i][j];
+			}
+		}
+		return d1Matrix;
+	}
+	
+	public double[][] melFilterBank(int numOfFilterBanks, int fftLength, int sampleRate)
+	{
+		double f0 = 700/(double)sampleRate;
+		int fn2 = (int)Math.floor((double)fftLength/2);
+		double lr = Math.log((double)1+0.5/f0)/(numOfFilterBanks+1);
+		double[] cfVector = arrange(1, numOfFilterBanks+1);
+		
+		for(int i=0;i<cfVector.length;i++){
+			cfVector[i] = sampleRate*f0*(Math.exp(cfVector[i]*lr)-1);
+		}
+		
+		double[] bl = {0, 1, fftLength, fftLength+1};
+		
+		for(int i=0;i<bl.length;i++){
+			bl[i] = fftLength*f0*(Math.exp(bl[i]*lr)-1);
+		}
+		
+		int b1 = (int)Math.floor(bl[0])+1;					//1
+		int b2 = (int)Math.ceil(bl[1]);						//1
+		int b3 = (int)Math.floor(bl[2]);					//fftLen
+		int b4 = Math.min(fn2, (int)Math.ceil(bl[3]))-1;
+		double[] pf = arrange(b1, b4+1);
+		
+		for(int i=0;i<pf.length;i++){
+			pf[i] = Math.log(1+pf[i]/f0/(double)fftLength)/lr;
+		}
+		
+		double[] fp = new double[pf.length];
+		double[] pm = new double[pf.length];
+		
+		for(int i=0;i<fp.length;i++){
+			fp[i] = Math.floor(pf[i]);
+			pm[i] = pf[i] - fp[i];
+		}
+		double[][] matrix = new double[numOfFilterBanks][1+fn2];
+		int r=0;
+		
+		for(int i=b2-1;i<b4;i++){
+			r = (int)fp[i]-1;
+			matrix[r][i+1] += 2*(1-pm[i]);
+		}
+		
+		for(int i=0;i<b3; i++){
+			r = (int)fp[i];
+			matrix[r][i+1] += 2* pm[i];
+		}
+		
+		double[] tempRow = null;
+		double rowEnergy = 0;
+		
+		for(int i=0;i<matrix.length;i++){
+			tempRow = matrix[i];
+			rowEnergy = energyOfVector(tempRow);
+			if(rowEnergy < 0.0001){
+				tempRow[i] = i;
+			}
+			else{
+				while(rowEnergy>1.01){
+					tempRow = matrixService.vectorMultiplyByScalar(tempRow, 0.99);
+					rowEnergy = energyOfVector(tempRow);
+				}
+				while(rowEnergy<0.99){
+					tempRow = matrixService.vectorMultiplyByScalar(tempRow, 1.01);
+					rowEnergy = energyOfVector(tempRow);
+				}
+			}
+			matrix[i] = tempRow;
+		}
+		return matrix;
+	}
+	
+	private double[] hamming(int frameLen){
+		double[] windowTemp = new double[frameLen];
+		for(int i=0;i<windowTemp.length;i++){
+			windowTemp[i] = 0.54-0.46*Math.cos(2*Math.PI/(double)frameLen*((double)i+0.5));
+		}
+		return windowTemp;
+	}
+	
+	private int setFrameLen(int sampleRate){
+		return (int) (0.025*(double)(sampleRate));
+	}
+	
+	private int setFrameShift(int sampleRate){
+		return (int) (0.0125*(double)(sampleRate));
+	}
+	
+	private void arrayLimitCheck(int low, int high) throws MfccServiceException {
+		if(low > high)
+		{
+			throw new MfccServiceException(wrongLimitsError);
+		}
+	}
+}
