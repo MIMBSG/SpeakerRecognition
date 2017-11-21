7575fc49c251b855532a30552d50efd6ed88d44e
Maciej <maciej.gbiorczyk@student.put.poznan.pl>
2017-11-21 12:17:15 +0100

    LabelsInertiaDistances - added hashCode and equals methods. MG

--- SpeakerRecognition/src/main/java/speakerrecognition/pojos/LabelsInertiaDistances.java
+++ SpeakerRecognition/src/main/java/speakerrecognition/pojos/LabelsInertiaDistances.java
@@ -1,5 +1,7 @@
 package speakerrecognition.pojos;
 
+import java.util.Arrays;
+
 public class LabelsInertiaDistances {
 
 	private double inertia;
@@ -23,6 +25,36 @@ public class LabelsInertiaDistances {
 
 	public double[] getLabels() {
 		return labels;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(distances);
+		long temp;
+		temp = Double.doubleToLongBits(inertia);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + Arrays.hashCode(labels);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		LabelsInertiaDistances other = (LabelsInertiaDistances) obj;
+		if (!Arrays.equals(distances, other.distances))
+			return false;
+		if (Double.doubleToLongBits(inertia) != Double.doubleToLongBits(other.inertia))
+			return false;
+		if (!Arrays.equals(labels, other.labels))
+			return false;
+		return true;
 	}
 
 }
